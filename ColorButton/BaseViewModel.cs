6cd3cd87d7261a8c3f215b862b84d1af03ac8554
HenryH <52370186+Zikun-Huang@users.noreply.github.com>
2021-06-15 20:53:49 +0800

    Add project files.

---
+++ ColorButton/BaseViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MvvmHelpers;
+using MvvmHelpers.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ColorButton
+{
+    public class BaseViewModel : ObservableObject
+    {
+        public BaseViewModel()
+        {
+            ChangeColor = new AsyncCommand(OnColorChange);
+        }
+
+        public ICommand ChangeColor { get; }
+
+        private Color buttonColor;
+
+        public Color ButtonColor
+        {
+            get => buttonColor;
+            set => SetProperty(ref buttonColor, value);
+        }
+
+        async Task OnColorChange()
+        {
+            await Task.Delay(100);
+            Random r = new Random();
+            int intButtonColorR = r.Next(256);
+            int intButtonColorG = r.Next(256);
+            int intButtonColorB = r.Next(256);
+            Color hexButtonColor = Color.FromRgb(intButtonColorR, intButtonColorG, intButtonColorB);
+            ButtonColor = hexButtonColor;
+        }
+    }
+}
